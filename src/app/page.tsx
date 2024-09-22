fd7deda5cf86ad2e3b7966aca0f2f58ab302a686
Amber <amberkhanum33@gmail.com>
2024-09-23 02:21:06 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div className="intro">
+      <div>
+        <h1 className="name">AMBER KHANUM</h1>
+        <h2 className="css">Computer Science Student</h2>
+      </div>
+
+      <div className="section">
+        <div className="edu">
+          <ul className="title1">Education:</ul>
+          <li>A'Levels</li>
+          <li> O'Levels</li>
+        </div>
+        <div className="skills">
+          <ul className="title2">Skills:</ul>
+          <li>Typescript</li>
+          <li>HTML/CSS</li>
+          <li>English Language</li>
+        </div>
+      </div>
+    </div>
+  );
+}
